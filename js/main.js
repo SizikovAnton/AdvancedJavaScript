765be3f66a92388adbae14af31b1f432de2fb75c
Anton <tosha.sizikov@gmail.com>
2019-07-11 20:00:05 +0300

    Изменил стили. Перевел все на классы.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,59 @@
-//Вопросы:
-//1. Мы объявляем функции через константы, что бы нельзя было их случайно изменить?
-//2. Почему мы объявляем массив объектов как константу?
+const DEFAULT_IMAGE = 'https://placehold.it/200x200';
 
-const products = [
-    {id: 1, title: 'Notebook', price: 2000, image: ''},
-    {id: 2, title: 'Keyboard', price: 200, image: ''},
-    {id: 3, title: 'Mouse', price: 47, image: ''},
-    {id: 4, title: 'Gamepad', price: 87, image: 'gamepad.jpg'},
-    {id: 5, title: 'Chair', price: 187, image: ''},
-    {id: 6, title: 'Chair 2', image: ''},
-    {id: 7, title: 'Chair 3', price: 187},
-    {id: 8, title: 'Chair 4', price: null},
-];
+class Products {
+    constructor(container = `.products`) {
+        this.container = container;
+        this.data = [];
+        this.allProduct = [];
+        this.init();
+    }
+    init() {
+        this._fetchProducts();
+        this._render();
+    }
+    _fetchProducts() {
+        this.data = [
+            { id: 1, title: 'Notebook', price: 2000, image: '' },
+            { id: 2, title: 'Keyboard', price: 200, image: '' },
+            { id: 3, title: 'Mouse', price: 47, image: '' },
+            { id: 4, title: 'Gamepad', price: 87, image: 'gamepad.jpg' },
+            { id: 5, title: 'Chair', price: 187, image: '' },
+            { id: 6, title: 'Chair 2', image: '' },
+            { id: 7, title: 'Chair 3', price: 187 },
+            { id: 8, title: 'Chair 4', price: null },
+        ];
+    }
+    _render() {
+        const block = document.querySelector(this.container);
+        for (let item of this.data) {
+            const product = new ProductItem(item);
+            this.allProduct.push(product);
+            block.insertAdjacentHTML('beforeend', product.render())
+        }
+    }
+}
 
-const renderProduct = (title, price = 'Цена не указана', image = 'default.png') => {
-    return `<div class="product-item">
-                <img src="img/${image.length ? image : 'default.png'}" alt="" class="product-item__img">
-                <h3>${title}</h3>
-                <p>${price ? price : 'Цена не указана'} ${typeof(price) == 'number' ? 'р.' : ''}</p>
-                <button class="buy-btn btn">Купить</button>
-            </div>`
-};
+class ProductItem {
+    constructor(item) {
+        this.id = item.id;
+        this.title = item.title;
+        this.price = item.price;
+        if (item.image) {
+            this.image = 'img/' + item.image;
+        } else {
+            this.image = DEFAULT_IMAGE;
+        }
 
-const renderPage = list => {
-    const productsList = list.map(el => renderProduct(el.title, el.price, el.image)); //Метод map возвращает массив
-    //Метод join преобразует массив в строку. 
-    //Разделитель элементов массива в строке можно указать как аргумент к методу join. 
-    //В нашем случае, что бы избавиться от запятой, мы используем пустую строку как разделитель.
-    document.querySelector(`.products`).innerHTML = productsList.join(''); 
-};
+    }
+    render() {
+        //console.log(this);
+        return `<div class="product-item">
+                    <img src="${this.image}" alt="" class="product-item__img">
+                    <h3>${this.title}</h3>
+                    <p>${this.price ? this.price : 'Цена не указана'} ${typeof(this.price) == 'number' ? 'р.' : ''}</p>
+                    <button class="buy-btn btn">Купить</button>
+                </div>`
+    }
+}
 
-renderPage(products);
+const products = new Products();
