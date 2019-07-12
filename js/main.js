6dfb4ca366c22b305500a9f051ff4317c41c4c99
Anton <tosha.sizikov@gmail.com>
2019-07-12 20:11:39 +0300

    Добавлена неполная реализация классов для корзины.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const DEFAULT_IMAGE = 'https://placehold.it/200x200';
 const API = `https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses`;
-
 //-----Без использования промисов-----
 // let getRequest = (url, cb) => {
 //     let xhr = new XMLHttpRequest();
@@ -45,7 +44,6 @@ const API = `https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-a
 // }, (error) => {
 //     console.log(`Ошибка ${error}`);
 // });
-
 
 class Products {
     constructor(container = `.products`) {
@@ -95,7 +93,7 @@ class ProductItem {
 
     }
     render() {
-        return `<div class="product-item">
+        return `<div class="product-item" data-id="${this.id_product}">
                     <img src="${this.image}" alt="" class="product-item__img">
                     <h3>${this.product_name}</h3>
                     <p>${this.price ? this.price : 'Цена не указана'} ${typeof(this.price) == 'number' ? 'р.' : ''}</p>
@@ -104,26 +102,99 @@ class ProductItem {
     }
 }
 
-// class Cart {
-//     constructor() {
-//         this.items //Массив с добавленными в корзину товарами (объекты класса CartItem)
-//         this.totalPrice //Общая стоимость корзины
-//         this.totalCount //Общее кол-во товаров в корзине
-//     }
-//     render() {} //Метод для отрисовки корзины. Во времня рендера подсчитывается общее кол-во и стоимость товаров, а так же удаляются из массива элементы, чей count = 0.
-//     clear() {} //Метод для полной отчистки корзины
-//     addItem() {} //Добавляем новую позицию в корзину
-//     deleteItem() {} //Удаляем позицию из корзины
+
+
+class Cart {
+    constructor() {
+        this.items = []; //Массив с добавленными в корзину товарами (объекты класса CartItem)
+        this.totalPrice = 0; //Общая стоимость корзины
+        this.totalCount = 0; //Общее кол-во товаров в корзине
+        this._setCart()
+            .then(() => this._render());
+    }
+    _setCart() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .then(inCart => {
+                this.totalPrice = inCart.amount;
+                this.totalCount = inCart.countGoods;
+                for (let item of inCart.contents) {
+                    this.items.push(new CartItem(item.id_product, item.product_name, item.price, item.quantity));
+                }
+            })
+            .catch(error => console.log(error))
+    }
+    _render() {
+        const block = document.querySelector(`.cart-content`);
+        let totalPrice = 0;
+        let totalCount = 0;
+        block.innerHTML = '';
+        for (let item of this.items) {
+            block.insertAdjacentHTML('beforeend', item.render());
+            totalCount += item.quantity;
+            totalPrice += item.price * item.quantity;
+        }
+        this.totalPrice = totalPrice;
+        this.totalCount = totalCount;
+        document.querySelector('#cartTotalCount').textContent = this.totalCount;
+        document.querySelector('#cartTotalPrice').textContent = this.totalPrice;
+
+    }
+    clear() {} //Метод для полной отчистки корзины
+    addItem() {} //Добавляем новую позицию в корзину
+    deleteItem() {} //Удаляем позицию из корзины
+    getItemById(id) {
+        let index = null;
+        this.items.forEach((item, i) => {
+            if (item.id_product == id) {
+                index = i;
+            }
+        });
+        return index;
+    }
+}
+
+class CartItem {
+    constructor(id_product, product_name, price, quantity) {
+        this.id_product = id_product;
+        this.product_name = product_name;
+        this.price = price;
+        this.quantity = quantity ? quantity : 1;
+    }
+    plusCount() {
+        this.quantity++;
+    }
+    minusCount() {
+        this.quantity--;
+    }
+    render() {
+        return `<div class="cart-row cart-element" data-id="${this.id_product}">
+            <div>${this.product_name}</div>
+            <div>${this.price}</div>
+            <div class="cart-quantity"><div class="btn btn-plus" onclick="btnPlus(${this.id_product})">+</div>${this.quantity}<div class="btn btn-minus" onclick="btnMinus(${this.id_product})">-</div></div>
+            <div>${this.price*this.quantity}</div>
+            <div class="btn btn-delete">Удалить</div>`;
+    }
+}
+
+// function cartElementClick(event) {
+//     event
+//     cart.items[cart.getItemById()]
 // }
 
-// class CartItem() {
-//     constructor(item) { //Конструктор принимает ссылку на экземпляр класса ProductItem
-//         this.link = item //Ссылка на экземпляр класса
-//         this.count = 1; //Колличество данного товара в корзине
-//     }
-//     plusCount() {} //Увеличение кол-ва на единицу
-//     minusCount() {} //Уменьшение кол-ва на единицу
-// }
+function btnPlus(id) {
+    cart.items[cart.getItemById(id)].plusCount();
+    cart._render();
+}
 
+function btnMinus(id) {
+    cart.items[cart.getItemById(id)].minusCount();
+    cart._render();
+}
+
+// function eventsAdd() {
+//     document.querySelectorAll('.cart-element').addEventListener('click', cartElementClick(event));
+// }
 const products = new Products();
-console.log(products.totalPrice());
+const cart = new Cart();
+//eventsAdd();
